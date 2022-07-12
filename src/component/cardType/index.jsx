d5dbaebd00cfb8c27618e0a47fb77a4ceba2be44
Senak9 <joaomarcerlo.o.rocha@gmai.com>
2022-07-12 11:37:00 -0300

    feat: cardtype blocking and home details

---
+++ src/component/cardType/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function CardType() {
+  return (
+    <div className="containerCardType">
+      <h1>Pokedex</h1>
+    </div>
+  );
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Header from "../../component/header";
+import CardType from "../../component/cardType";
 import "./style.css";
+import PokemonHome from "../../assets/pokemonHome.svg";
 
 export default function Home() {
   return (
     <div className="containerHome">
       <Header />
       <div className="boxHome">
-        <p>Home</p>
+        <div className="boxCard">
+          <CardType /> <CardType />
+        </div>
+        <div className="boxCard">
+          <CardType /> <CardType />
+        </div>
+        <div>
+          <img src={PokemonHome} alt="PokemonHome" className="pokemonHome" />
+        </div>
       </div>
     </div>
   );
